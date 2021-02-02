848ac898472475f9b07969157fafef7c89de9243
jeon3029 <jeon3029@naver.com>
2021-02-02 17:19:06 +0900

    labeling of graph

---
+++ labeling.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [0, 1, 2, 3, 4]
+y = [ [0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [9, 8, 7, 6, 5] ]
+labels = ['foo', 'bar', 'baz']
+
+for y_arr, label in zip(y, labels):
+    plt.plot(x, y_arr, label=label)
+
+plt.legend()
+plt.show()
